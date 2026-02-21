9e32a4b3184dfbe3426a86edabace2fbfa378d95
Mohamed717 <mohamed717saleh@gmail.com>
2026-02-21 13:27:54 +0200

    Intgerate quadric solver with ggplot2(library)

--- R/base_code.R
+++ R/base_code.R
@@ -18,7 +18,9 @@ m_sin <- function(x){return(sin(x))}
 m_cos <- function(x){return(cos(x))}
 m_tan <- function(x){return(tan(x))}
 
-
+power <- function(x, y) {
+  return(x^y)
+}
 
 # حساب القيمة المطلقة
 m_abs <- function(x) { return(abs(x)) }
@@ -51,3 +53,41 @@ else {
   return(c(x1,x2))
   
 }       } 
+                      #ggplot2
+                    library(ggplot2)
+                          draw_plot<-function(eq_string,x1=NULL,x2=NULL){
+                          # تحويل النص لدالة رياضية
+                                my_func<-function(x){
+                                  eval(parse(text = eq_string))}
+                                        
+                                      p <- ggplot() +
+                                        xlim(-10,10) +
+                                          geom_function(fun=my_func,color="blue",linewidth =1) +
+                                            theme_minimal()
+                                      # لو فيه حلول (نقط)، ضيفها على الرسمة كلون أحمر
+                          if(!is.na(x1)){
+                            p <- p + geom_point(aes(x=x1,y=0),color="red",size=4)
+                            
+                          }
+                                      if(!is.na(x1)){
+                                        p <- p + geom_point(aes(x=x2,y=0),color="red",size=4)
+                                        
+                                      }
+                                      return(p)
+                                
+                          } 
+                          
+                          
+                          #Quadric_solver + ggplot2
+                            
+                              process_the_equation <- function(a,b,c) {
+                                #تكوين النص بتاع المعادلة عشان الرسم
+                                    eq_text <- paste0(a, "*x^2 + ", b, "*x + ", c)
+                                        
+                                      #Quadric_solver
+                                    solution <- Quadric_solver(a,b,c)
+                                          #ggplot2
+                                         plot_f <- draw_plot(eq_text,solution[1],solution[2])
+                                         return(plot_f)
+                                
+                                 }
