78647b4bee4ea441ca7613bf06e7455cc8bd5ac1
Y0sefTamer <programmeryosef@gmail.com>
2026-02-22 05:26:45 +0200

    Added the ui and fix some error

--- R/base_code.R
+++ R/base_code.R
@@ -1,53 +1,51 @@
+# الدوال الحسابية البسيطة
 add <- function(x, y) { return(x + y) }
+
 subtract <- function(x, y) { return(x - y) }
+
 multiply <- function(x, y) { return(x * y) }
-divide <- function(x, y) { if (y==0)return("Math Error")
-  return(x / y) }
-m_sqrt <- function(x) {
+
+divide <- function(x, y) { 
+  if (y == 0) return("Math Error")
+  return(x / y) 
+}
+
+Remainder <- function(a, b) {
+  if (b == 0) {
+    return("Error: Division by zero is not allowed!")
+  }
+  return(a %% b)
+}
+
+# الدوال العلميه
+Sqrt <- function(x) {
   if (x < 0) return("Error: Negative Input")
-  return(sqrt(x))
+  return(base::sqrt(x))
 }
 
-m_log <- function(x){if(x<=0)return("Error: log of 0 or Negative")
-  return(log(x))}
+Log <- function(x) {
+  if (x <= 0) return("Error: log of 0 or Negative")
+  return(base::log10(x))
+}
 
+Ln <- function(x) {
+  if (x <= 0) return("Error: ln of 0 or Negative")
+  return(base::log(x))
+}
 
+Square <- function(x) { return(x^2) }
 
-# الدوال المثلثية (بتحول للراديان تلقائياً)
-m_sin <- function(x){return(sin(x))}
-m_cos <- function(x){return(cos(x))}
-m_tan <- function(x){return(tan(x))}
+# الدوال المثلثيه
+Sin <- function(x) { return(base::sin(x * pi / 180)) }
+Cos <- function(x) { return(base::cos(x * pi / 180)) }
+Tan <- function(x) { return(base::tan(x * pi / 180)) }
 
+# abs
+Abs <- function(x) { return(base::abs(x)) }
 
+#factorial
 
-# حساب القيمة المطلقة
-m_abs <- function(x) { return(abs(x)) }
-
-
-
-# حساب المضروب (Factorial)
-m_factorial <- function(x) {
+Fact <- function(x) {
   if (x < 0) return("Error: Negative Factorial")
-  return(factorial(as.integer(x)))
+  return(base::factorial(as.integer(x)))
 }
-
-
-
-#حل المعادلات التربيعية
-Quadric_solver <- function(a,b,c){D <- b^2-4*a*c
-#يوجد حلين حقيقيين
-if(D>0){x1 <- (-b+sqrt(D))/(2*a)
-x2 <- (-b-sqrt(D))/(2*a)
-return(c(x1,x2))
-}
-#يوجد حل واحد
-else if(D==0){
-  x1 <- -b/(2*a)
-  return(x1)}
-#تخيلية  
-else {
-  x1 <- (-b+sqrt(as.complex(D)))/(2*a)
-  x2 <- (-b-sqrt(as.complex(D)))/(2*a)
-  return(c(x1,x2))
-  
-}       } 
--- app/app.R
+++ app/app.R
@@ -1,51 +1,128 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
 library(shiny)
-
-# Define UI for application that draws a histogram
+#source("R/base_code.R")
+# ================= UI =================
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "style.css")
+  ),
+  #includeCSS("style.css"),
+  
+  div(class = "calculator",
+      
+      div(class = "brand", "COMPLEX ZERO"),
+      div(class = "display", textOutput("display")),
+      
+      
+      div(class="row",
+          actionButton("clear", "C", class="btn"),
+          actionButton("sqrt", "√", class="btn operator"),
+          actionButton("square", "x²", class="btn operator"),
+          actionButton("qous",")",class="btn"),
+          actionButton("back", "⌫", class="btn ")
+      ),
+      
+      
+      div(class="row",
+          actionButton("sin", "sin", class="btn operator"),
+          actionButton("cos", "cos", class="btn operator"),
+          actionButton("tan", "tan", class="btn operator"),
+          actionButton("log", "log", class="btn operator"),
+          actionButton("mod", "%", class="btn operator"),
+      ),
+      
+      
+      div(class="row",
+          actionButton("abs", "abs", class="btn operator"),
+          actionButton("fact", "!", class="btn operator"),
+          actionButton("pow", "^", class="btn operator"),
+          actionButton("add", "+", class="btn operator")
+      ),
+      
+      
+      div(class="row",
+          actionButton("7", "7", class="btn"),
+          actionButton("8", "8", class="btn"),
+          actionButton("9", "9", class="btn"),
+          actionButton("mult", "*", class="btn operator")
+      ),
+      div(class="row",
+          actionButton("4", "4", class="btn"),
+          actionButton("5", "5", class="btn"),
+          actionButton("6", "6", class="btn"),
+          actionButton("div", "/", class="btn operator")
+      ),
+      div(class="row",
+          actionButton("1", "1", class="btn"),
+          actionButton("2", "2", class="btn"),
+          actionButton("3", "3", class="btn"),
+          actionButton("sub", "-", class="btn operator")
+      ),
+      div(class="row",
+          actionButton("0", "0", class="btn"),
+          actionButton("dot", ".", class="btn"),
+          actionButton("ln", "ln", class="btn operator"),
+          actionButton("equal", "=", class="btn equal")
+      )
+  )
 )
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
+# ================= SERVER =================
+server <- function(input, output, session) {
+  
+  expr <- reactiveVal("")
+  
+  output$display <- renderText({ expr() })
+  
+  # Clear
+  observeEvent(input$clear, { expr("") })
+  
+  # Numbers
+  lapply(0:9, function(i){
+    observeEvent(input[[as.character(i)]], {
+      expr(paste0(expr(), i))
     })
+  })
+  
+  # back
+  observeEvent(input$back,{
+    current <- expr()
+    if(nchar(current) > 0){
+      expr(substr(current,1,nchar(current) -1))
+    }
+  }
+  )
+  
+  # Dot
+  observeEvent(input$dot, { expr(paste0(expr(), ".")) })
+  observeEvent(input$qous, { expr(paste0(expr(), ")")) })
+  
+  # Operators
+  observeEvent(input$add, { expr(paste0(expr(), "+")) })
+  observeEvent(input$sub, { expr(paste0(expr(), "-")) })
+  observeEvent(input$mult, { expr(paste0(expr(), "*")) })
+  observeEvent(input$div, { expr(paste0(expr(), "/")) })
+  observeEvent(input$mod, { expr(paste0(expr(), "%%")) })
+  observeEvent(input$pow, { expr(paste0(expr(), "^")) })
+  observeEvent(input$fact, { expr(paste0(expr(),"Fact("))})
+  observeEvent(input$square, { expr(paste0(expr(),"^2"))})
+  observeEvent(input$ln, { expr(paste0(expr(),"Ln("))})
+  observeEvent(input$abs, { expr(paste0(expr(),"Abs("))})
+  
+  # Scientific Functions
+  observeEvent(input$sqrt, { expr(paste0(expr(), "Sqrt(")) })
+  observeEvent(input$sin,  { expr(paste0(expr(), "Sin(")) })
+  observeEvent(input$cos,  { expr(paste0(expr(), "Cos(")) })
+  observeEvent(input$tan,  { expr(paste0(expr(), "Tan(")) })
+  observeEvent(input$log,  { expr(paste0(expr(), "Log(")) })
+  
+  # Equal
+  observeEvent(input$equal, {
+    tryCatch({
+      result <- eval(parse(text = expr()))
+      expr(as.character(result))
+    }, error = function(e){
+      expr("Error")
+    })
+  })
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
